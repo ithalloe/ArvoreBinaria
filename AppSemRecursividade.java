6111995bc046fe3b775c603aaa80b3f8862fa4f0
ithalloe <145879813+ithalloe@users.noreply.github.com>
2025-05-23 10:04:47 -0300

    Create AppSemRecursividade.java

---
+++ AppSemRecursividade.java
@@ -0,0 +1,39 @@
+public class App {
+    public static void main(String[] args) {
+        Arvore arvore = new Arvore();
+        int total = arvore.contarNos();
+        System.out.println("Total de nós: " + total);
+    }
+}
+public class App {
+    public static void main(String[] args) {
+        Arvore arvore = new Arvore();
+        System.out.println("Percurso em ordem:");
+        arvore.percorrerEmOrdem();
+    }
+  
+public class App {
+    public static void main(String[] args) {
+        Arvore arvore = new Arvore();
+        System.out.print("Percurso Pré-Ordem: ");
+        arvore.percorrerPreOrdem();
+    }
+}
+ public class App {
+   public static void main(String[] args) {
+        Arvore arvore = new Arvore();
+        System.out.print("Percurso Pos-Ordem: ");
+        arvore.percorrerPosOrdem();
+    }
+}
+  public class App {
+    public static void main(String[] args) {
+        Arvore arvore = new Arvore();
+        System.out.print("Percurso em Largura: ");
+        arvore.percorrerEmLargura();
+    }
+}
+
+// Favor quando for testar, usar separadamente a parte desejada em conjunto com o metodo da classe Arvore 
+//Ithallo Emanuel
+
