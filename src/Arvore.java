5087e49c122c708860b4f0ecbad255e04acdc903
ithalloe <145879813+ithalloe@users.noreply.github.com>
2025-05-27 20:16:46 -0300

    Update Arvore.java - Contar Nós Folha

--- src/Arvore.java
+++ src/Arvore.java
@@ -80,5 +80,17 @@ public void imprimirNivel(No no, int nivel) {
     imprimirNivel(raiz, i);
      }
     }
-   }
+ public int contarNosFolha() {
+    return contarNosFolha(raiz);
+}
+ public int contarNosFolha(No no) {
+    if (no == null) {
+        return 0;
+    }
+    if (no.esquerda == null && no.direita == null) {
+        return 1;
+    }
+    return contarNosFolha(no.esquerda) + contarNosFolha(no.direita);
+ }
+}
 
